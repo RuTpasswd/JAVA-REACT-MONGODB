202d32a8180332dbcb10ff708fe7fc5f6eb846a4
ganzamick <ganzamick@gmail.com>
2023-06-23 00:46:33 +0200

    Initials

---
+++ movies/src/test/java/ganza/mick/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package ganza.mick.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
